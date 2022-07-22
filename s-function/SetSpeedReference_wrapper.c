c41abd76995f78ea9f736cc758fbbc78e20523ee
Solo-FL <f.liuzzi@solomotorcontrollers.com>
2022-07-22 14:28:16 +0200

    update

---
+++ s-function/SetSpeedReference_wrapper.c
@@ -0,0 +1,83 @@
+
+/*
+ * Include Files
+ *
+ */
+#if defined(MATLAB_MEX_FILE)
+#include "tmwtypes.h"
+#include "simstruc_types.h"
+#else
+#include "rtwtypes.h"
+#endif
+
+
+
+/* %%%-SFUNWIZ_wrapper_includes_Changes_BEGIN --- EDIT HERE TO _END */
+#include <windows.h>
+/* %%%-SFUNWIZ_wrapper_includes_Changes_END --- EDIT HERE TO _BEGIN */
+#define u_width 1
+#define y_width 1
+
+/*
+ * Create external references here.  
+ *
+ */
+/* %%%-SFUNWIZ_wrapper_externs_Changes_BEGIN --- EDIT HERE TO _END */
+/* extern double func(double a); */
+/* %%%-SFUNWIZ_wrapper_externs_Changes_END --- EDIT HERE TO _BEGIN */
+
+/*
+ * Start function
+ *
+ */
+void SetSpeedReference_Start_wrapper(void **pW)
+{
+/* %%%-SFUNWIZ_wrapper_Start_Changes_BEGIN --- EDIT HERE TO _END */
+/*
+ * Custom Start code goes here.
+ */
+    
+pW[0] = LoadLibrary("SoloDLL_Sim.dll");
+pW[1] = GetProcAddress(pW[0], "_SetSpeedReference");
+/* %%%-SFUNWIZ_wrapper_Start_Changes_END --- EDIT HERE TO _BEGIN */
+}
+/*
+ * Output function
+ *
+ */
+void SetSpeedReference_Outputs_wrapper(const uint8_T *Address,
+			const int32_T *Value,
+			boolean_T *Result,
+			int32_T *Error,
+			void **pW)
+{
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_BEGIN --- EDIT HERE TO _END */
+/* This sample sets the output equal to the input
+      y0[0] = u0[0]; 
+ For complex signals use: y0[0].re = u0[0].re; 
+      y0[0].im = u0[0].im;
+      y1[0].re = u1[0].re;
+      y1[0].im = u1[0].im;
+ */
+    
+boolean (*func)(uint8_T, int32_T, int32_T*);
+func = pW[1];
+Result[0] = func(Address[0], Value[0], Error);
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_END --- EDIT HERE TO _BEGIN */
+}
+
+/*
+ * Terminate function
+ *
+ */
+void SetSpeedReference_Terminate_wrapper(void **pW)
+{
+/* %%%-SFUNWIZ_wrapper_Terminate_Changes_BEGIN --- EDIT HERE TO _END */
+/*
+ * Custom Terminate code goes here.
+ */
+    
+FreeLibrary(pW[0]);
+/* %%%-SFUNWIZ_wrapper_Terminate_Changes_END --- EDIT HERE TO _BEGIN */
+}
+
